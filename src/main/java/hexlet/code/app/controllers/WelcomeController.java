ea68a8fc316f130eedda4b4d3b7c0f4aebb9cc34
Ivan_Vyargizov <vyargizov@gmail.com>
2022-06-04 17:44:27 +0400

    update

--- src/main/java/hexlet/code/app/controllers/WelcomeController.java
+++ src/main/java/hexlet/code/app/controllers/WelcomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
 
     @GetMapping("/welcome")
-    public String welcome() {
+    public final String welcome() {
         return "Welcome to Spring";
     }
 
