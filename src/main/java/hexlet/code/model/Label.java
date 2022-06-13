f1d6f729d254e7430e7b6722b3c6816fdc79ea5e
Ivan_Vyargizov <vyargizov@gmail.com>
2022-06-13 20:21:13 +0400

    add Label Entity

---
+++ src/main/java/hexlet/code/model/Label.java
@@ -0,0 +1,31 @@
+package hexlet.code.model;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "labels")
+public class Label {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank
+    @Size(min = 1, max = 100)
+    private String name;
+
+    @CreationTimestamp
+    private Date createdAt;
+
+}
