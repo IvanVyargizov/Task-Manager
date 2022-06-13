df0bbebf50c95a8f2982aea5c5f7dd78a98c8bba
Ivan_Vyargizov <vyargizov@gmail.com>
2022-06-13 20:20:57 +0400

    add Task Entity

---
+++ src/main/java/hexlet/code/model/Task.java
@@ -0,0 +1,72 @@
+package hexlet.code.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Date;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Entity
+@Builder
+@Table(name = "tasks")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private long id;
+
+    @NotBlank
+    @Size(min = 1)
+    private String name;
+
+    private String description;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "task_status_id")
+    private TaskStatus taskStatus;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private User author;
+
+    @ManyToOne
+    @JoinColumn(name = "executor_id")
+    private User executor;
+
+    @CreationTimestamp
+    private Date createdAt;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "task_labels",
+            joinColumns = @JoinColumn(name = "task_id"),
+            inverseJoinColumns = @JoinColumn(name = "labels_id"))
+    @Builder.Default
+    private Set<Label> labels = new LinkedHashSet<>();
+
+}
