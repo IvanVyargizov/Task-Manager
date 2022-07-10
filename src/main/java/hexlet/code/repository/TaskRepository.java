92747c368ef2e8a5acdbccbdaa1c1e399df060cd
Ivan_Vyargizov <vyargizov@gmail.com>
2022-07-10 22:08:06 +0400

    update

--- src/main/java/hexlet/code/repository/TaskRepository.java
+++ src/main/java/hexlet/code/repository/TaskRepository.java
@@ -1,26 +1,11 @@
 package hexlet.code.repository;
 
 import hexlet.code.model.Task;
-import hexlet.code.model.QTask;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
-import org.springframework.data.querydsl.binding.QuerydslBindings;
+import org.springframework.stereotype.Repository;
 
-public interface TaskRepository extends JpaRepository<Task, Long>,
-        QuerydslPredicateExecutor<Task>, QuerydslBinderCustomizer<QTask> {
-
-    @Override
-    default void customize(QuerydslBindings bindings, QTask task) {
-
-        bindings.bind(task.taskStatus.id).first(
-                (path, value) -> path.eq(value));
-        bindings.bind(task.executor.id).first(
-                (path, value) -> path.eq(value));
-        bindings.bind(task.labels.any().id).first(
-                (path, value) -> path.eq(value));
-        bindings.bind(task.author.id).first(
-                (path, value) -> path.eq(value));
-    }
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long>, QuerydslPredicateExecutor<Task> {
 
 }
