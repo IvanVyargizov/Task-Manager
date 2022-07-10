e08174db328b28e3a258e546571dd491c87c9699
Ivan_Vyargizov <vyargizov@gmail.com>
2022-07-10 21:53:14 +0400

    split into separate tests

--- src/test/java/hexlet/code/controller/TaskControllerTests.java
+++ src/test/java/hexlet/code/controller/TaskControllerTests.java
@@ -6,7 +6,10 @@ import hexlet.code.dto.TaskDto;
 import hexlet.code.model.Task;
 import hexlet.code.repository.TaskRepository;
 import hexlet.code.utils.TestUtils;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -34,6 +37,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @AutoConfigureMockMvc
 @ActiveProfiles(TEST_PROFILE)
 @ExtendWith(SpringExtension.class)
@@ -46,20 +50,21 @@ public class TaskControllerTests {
     @Autowired
     private TestUtils utils;
 
-    public TaskControllerTests() {
+    @BeforeAll
+    public void createTask() throws Exception {
+        utils.regDefaultUser();
+        utils.createDefaultTaskStatus();
     }
 
     @Test
-    public void crudTask() throws Exception {
-        //create
-        utils.tearDown();
+    public void create() throws Exception {
         assertEquals(0, taskRepository.count());
-        utils.regDefaultUser();
-        utils.createDefaultTaskStatus();
         utils.createDefaultTask().andExpect(status().isCreated());
         assertEquals(1, taskRepository.count());
+    }
 
-        //getAll
+    @Test
+    public void getAll() throws Exception {
         final var response1 = utils.perform(get(TASK_CONTROLLER_PATH), TEST_USERNAME)
                 .andExpect(status().isOk())
                 .andReturn()
@@ -67,8 +72,10 @@ public class TaskControllerTests {
         final List<Task> tasks = fromJson(response1.getContentAsString(), new TypeReference<>() {
         });
         assertThat(tasks).hasSize(1);
+    }
 
-        //getById
+    @Test
+    public void getById() throws Exception {
         final Task expectedTask = taskRepository.findAll().get(0);
         final var response2 = utils.perform(
                 get(TASK_CONTROLLER_PATH + ID, expectedTask.getId()), TEST_USERNAME)
@@ -82,8 +89,10 @@ public class TaskControllerTests {
         assertEquals(expectedTask.getDescription(), task.getDescription());
         assertEquals(expectedTask.getAuthor().getEmail(), task.getAuthor().getEmail());
         assertEquals(expectedTask.getTaskStatus().getId(), task.getTaskStatus().getId());
+    }
 
-        //update
+    @Test
+    public void update() throws Exception {
         final Long statusId = taskRepository.findAll().get(0).getId();
         final var taskDto = new TaskDto(
                 TEST_TASK_NAME_2,
@@ -100,11 +109,77 @@ public class TaskControllerTests {
         assertEquals(taskRepository.findById(statusId).get().getName(), TEST_TASK_NAME_2);
         assertEquals(taskRepository.findById(statusId).get().getDescription(), TEST_TASK_DESCRIPTION_2);
         assertEquals(1, taskRepository.count());
+    }
 
-        //delete
+    @Test
+    public void deleteTask() throws Exception {
+        final Long statusId = taskRepository.findAll().get(0).getId();
         utils.perform(delete(TASK_CONTROLLER_PATH + ID, statusId), TEST_USERNAME)
                 .andExpect(status().isOk());
         assertEquals(0, taskRepository.count());
     }
+
+    @AfterAll
+    public void clear() {
+        utils.tearDown();
+    }
+
+//    @Test
+//    public void crudTask() throws Exception {
+//        //create
+//        utils.tearDown();
+//        assertEquals(0, taskRepository.count());
+//        utils.regDefaultUser();
+//        utils.createDefaultTaskStatus();
+//        utils.createDefaultTask().andExpect(status().isCreated());
+//        assertEquals(1, taskRepository.count());
+//
+//        //getAll
+//        final var response1 = utils.perform(get(TASK_CONTROLLER_PATH), TEST_USERNAME)
+//                .andExpect(status().isOk())
+//                .andReturn()
+//                .getResponse();
+//        final List<Task> tasks = fromJson(response1.getContentAsString(), new TypeReference<>() {
+//        });
+//        assertThat(tasks).hasSize(1);
+//
+//        //getById
+//        final Task expectedTask = taskRepository.findAll().get(0);
+//        final var response2 = utils.perform(
+//                get(TASK_CONTROLLER_PATH + ID, expectedTask.getId()), TEST_USERNAME)
+//                .andExpect(status().isOk())
+//                .andReturn()
+//                .getResponse();
+//        final Task task = fromJson(response2.getContentAsString(), new TypeReference<>() {
+//        });
+//        assertEquals(expectedTask.getId(), task.getId());
+//        assertEquals(expectedTask.getName(), task.getName());
+//        assertEquals(expectedTask.getDescription(), task.getDescription());
+//        assertEquals(expectedTask.getAuthor().getEmail(), task.getAuthor().getEmail());
+//        assertEquals(expectedTask.getTaskStatus().getId(), task.getTaskStatus().getId());
+//
+//        //update
+//        final Long statusId = taskRepository.findAll().get(0).getId();
+//        final var taskDto = new TaskDto(
+//                TEST_TASK_NAME_2,
+//                TEST_TASK_DESCRIPTION_2,
+//                1L,
+//                1L,
+//                null
+//        );
+//        final var updateRequest = put(TASK_CONTROLLER_PATH + TaskStatusController.ID, statusId)
+//                .content(asJson(taskDto))
+//                .contentType(APPLICATION_JSON);
+//        utils.perform(updateRequest, TEST_USERNAME).andExpect(status().isOk());
+//        assertTrue(taskRepository.existsById(statusId));
+//        assertEquals(taskRepository.findById(statusId).get().getName(), TEST_TASK_NAME_2);
+//        assertEquals(taskRepository.findById(statusId).get().getDescription(), TEST_TASK_DESCRIPTION_2);
+//        assertEquals(1, taskRepository.count());
+//
+//        //deleteTask
+//        utils.perform(delete(TASK_CONTROLLER_PATH + ID, statusId), TEST_USERNAME)
+//                .andExpect(status().isOk());
+//        assertEquals(0, taskRepository.count());
+//    }
 
 }
