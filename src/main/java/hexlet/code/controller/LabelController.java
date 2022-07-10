3af15b2d85ec59a4340d3ae35168a6927206f240
Ivan_Vyargizov <vyargizov@gmail.com>
2022-07-10 22:12:33 +0400

    code improvement

--- src/main/java/hexlet/code/controller/LabelController.java
+++ src/main/java/hexlet/code/controller/LabelController.java
@@ -3,11 +3,13 @@ package hexlet.code.controller;
 import hexlet.code.dto.LabelDto;
 import hexlet.code.model.Label;
 import hexlet.code.repository.LabelRepository;
+import hexlet.code.service.LabelService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,6 +27,7 @@ import java.util.List;
 import static hexlet.code.controller.LabelController.LABEL_CONTROLLER_PATH;
 import static org.springframework.http.HttpStatus.CREATED;
 
+@SecurityRequirement(name = "javainuseapi")
 @AllArgsConstructor
 @RestController
 @RequestMapping("${base-url}" + LABEL_CONTROLLER_PATH)
@@ -34,16 +37,15 @@ public class LabelController {
     public static final String ID = "/{id}";
 
     private final LabelRepository labelRepository;
+    private final LabelService labelService;
 
 
     @Operation(summary = "Create new label")
     @ApiResponse(responseCode = "201", description = "label created")
     @PostMapping
     @ResponseStatus(CREATED)
-    public Label registerNew(@RequestBody @Valid final LabelDto dto) {
-        Label label = new Label();
-        label.setName(dto.getName());
-        return labelRepository.save(label);
+    public Label createNewLabel(@RequestBody @Valid final LabelDto dto) {
+        return labelService.createNewLabel(dto);
     }
 
 
@@ -57,17 +59,21 @@ public class LabelController {
                 .toList();
     }
 
-    @ApiResponses(@ApiResponse(responseCode = "200"))
+    @Operation(summary = "Get label by Id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Label found"),
+            @ApiResponse(responseCode = "404", description = "Label with that id not found")
+    })
     @GetMapping(ID)
     public Label getTaskStatusById(@PathVariable final Long id) {
         return labelRepository.findById(id).get();
     }
 
+    @Operation(summary = "Update Label")
+    @ApiResponse(responseCode = "200", description = "Label updated")
     @PutMapping(ID)
     public Label update(@PathVariable final long id, @RequestBody @Valid final LabelDto dto) {
-        final Label labelToUpdate = labelRepository.findById(id).get();
-        labelToUpdate.setName(dto.getName());
-        return labelRepository.save(labelToUpdate);
+        return labelService.updateLabel(id, dto);
     }
 
     @DeleteMapping(ID)
