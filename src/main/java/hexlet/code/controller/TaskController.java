32a312a9b34c386024ea073b8d0ae557be787a7e
Ivan_Vyargizov <vyargizov@gmail.com>
2022-07-14 00:15:55 +0400

    remove unused parameters

--- src/main/java/hexlet/code/controller/TaskController.java
+++ src/main/java/hexlet/code/controller/TaskController.java
@@ -13,6 +13,9 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.AllArgsConstructor;
 import org.springframework.data.querydsl.binding.QuerydslPredicate;
 import org.springframework.security.access.prepost.PreAuthorize;
+
+import javax.validation.Valid;
+import com.querydsl.core.types.Predicate;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,9 +25,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
-import com.querydsl.core.types.Predicate;
 
 import static hexlet.code.controller.TaskController.TASK_CONTROLLER_PATH;
 import static org.springframework.http.HttpStatus.CREATED;
